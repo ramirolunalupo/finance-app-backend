54ccb42a35a190e7fa0bce45e04f180833991d61
ramirolunalupo <ramirolunalupo@gmail.com>
2025-12-14 19:40:23 -0300

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+# app/models/__init__.py
+from .user import User
+from .account import Account
+from .currency import Currency
+from .operation_type import OperationType
+from .operation import Operation
+from .journal_entry import JournalEntry
+from .journal_entry_line import JournalEntryLine
